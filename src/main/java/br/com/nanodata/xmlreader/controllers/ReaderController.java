ddbf7cb04b0d81f93b672a4d117760b859302d55
Arthur Bicego <arthurbq5@gmail.com>
2024-02-06 16:34:48 -0300

    feature/save - controller criado

--- src/main/java/br/com/nanodata/xmlreader/controllers/ReaderController.java
+++ src/main/java/br/com/nanodata/xmlreader/controllers/ReaderController.java
@@ -1,8 +1,12 @@
 package br.com.nanodata.xmlreader.controllers;
 
+import br.com.nanodata.xmlreader.models.dtos.SaveRequestDTO;
 import br.com.nanodata.xmlreader.services.ReaderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/reader")
@@ -10,4 +14,9 @@ public class ReaderController {
 
     @Autowired
     private ReaderService readerService;
+
+    @PostMapping("/save")
+    public SaveRequestDTO save(@RequestParam("files") List<MultipartFile> files) {
+        return readerService.processFile(files);
+    }
 }
---
+++ src/main/java/br/com/nanodata/xmlreader/models/dtos/SaveRequestDTO.java
@@ -0,0 +1,13 @@
+package br.com.nanodata.xmlreader.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SaveRequestDTO {
+
+    private String message;
+}
